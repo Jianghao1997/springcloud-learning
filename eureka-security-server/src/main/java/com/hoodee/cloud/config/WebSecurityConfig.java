5e890cfb1ceabdba37b61406acbf76b1e76e3ab1
jh <sukhao@163.com>
2022-09-14 10:58:19 +0800

    Eureka 组件学习

---
+++ eureka-security-server/src/main/java/com/hoodee/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.hoodee.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @version 1.0
+ * @author: jianghao
+ * @createTime: 2022年09月14日 10:37
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
